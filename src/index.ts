3037d62c3337364127d0e9769b9cce5845b12a8f
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-05-14 06:27:15 -0400

    Begining of something great

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Injector, Logger, webpack, FluxDispatcher } from "replugged";
 
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+class GuildProfile {
+  private inject = new Injector();
+  private logger = Logger.plugin("GuildProfile");
+
+
+  FluxDispatcher.subscribe()
+}
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
+  
 }
 
 export function stop(): void {
-  inject.uninjectAll();
 }
