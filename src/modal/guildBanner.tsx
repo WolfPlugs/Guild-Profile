26fe6e32ac20084afdfd5e824127efd265887e79
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-05-29 00:26:29 -0400

    start of Guild Banner

---
+++ src/modal/guildBanner.tsx
@@ -0,0 +1,20 @@
+import { common, componentsm webpack } from 'replugged'
+
+const { React } = common
+const { Tooltip } = components
+
+const { Colors } = await webpack.waitForModule<{ Colors: any }>(webpack.filters.byProps('Colors'))
+
+const [color, setColor] = React.useState(Colors.BRAND)
+const [hovered, setHovered] = React.useState(false)
+
+export const GuildBanner = (props) => {
+  const { guild } = props
+  function async componentDidMount() {
+    if (guild.icon) {
+      // const palette = await 
+    }
+  }
+
+
+}
